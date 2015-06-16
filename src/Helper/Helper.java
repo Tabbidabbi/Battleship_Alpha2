b873fa04aeb049b3f489a8d60e8685ff76f958c1
Tabbidabbi <winkler.bremen@hotmail.com>
2015-06-16 22:12:19 +0200

    funtion überladen

--- src/Helper/Helper.java
+++ src/Helper/Helper.java
@@ -37,4 +37,39 @@ public class Helper {
         } while (error);
         return input;
     }
+     public static int checkUserInput(int min, int max) {
+        boolean error = false;
+        do {
+            input = IO.readInt();
+            if (input < min || input > max) {
+                IO.println("Eingabe, außerhalb des gültigen Bereiches (" + min + "-" + max + ")");
+                error = true;
+            } else {
+                error = false;
+
+            }
+        } while (error);
+        return input;
+    }
+     
+        public static boolean checkOrientation() {
+        boolean orientation = false;
+        boolean error = false;
+        do {
+            IO.print("Bitte geben Sie die Ausrichtung des Schiffes an (h = horizontal, v = vertical: ");
+            String o = IO.readString();
+            if (o.equals("h")) {
+                error = false;
+                orientation = true;
+            } else if (o.equals("v")) {
+                error = false;
+                orientation = false;
+            } else {
+                IO.println("Falsche Eingabe, bitte wiederholen Sie die Eingabe!");
+                error = true;
+            }
+        } while (error);
+
+        return orientation;
+    }
 }
